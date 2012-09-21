5232df8b9c297d3c4b6f2fbd79e3c650524905fe
caldo_balon@hotmail.com <1234Qwe*>
2012-09-21 10:33:57 -0500

    agregue comentarios
    solo estamos probando

--- ProyectoWebPrueba/Default.aspx.cs
+++ ProyectoWebPrueba/Default.aspx.cs
@@ -12,7 +12,9 @@ namespace ProyectoWebPrueba
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
-            { 
+            {
+                //hola
+                //probando
             }
         }
     }
